208daae7159fb0e31d061acbe3cc3b3ff8675bc1
Ivan Paskhin <ipaskhin@captiv8.io>
2024-07-28 22:39:38 -0400

    refactor: optimize tax calculation logic
    (cherry picked from commit cfd23d8fd4861bc27b60f27174c6a0dedce986e1)

--- src/main/scala/TaxCalculator.scala
+++ src/main/scala/TaxCalculator.scala
@@ -1,15 +1,35 @@
+import scala.util.control.Breaks._
+
 object TaxCalculator {
 
-  def calculateTax(income: Double): Double = {
-    income match {
-      case _ if income <= 23200 => income * 0.10
-      case _ if income <= 94300 => (23200 * 0.10) + ((income - 23200) * 0.12)
-      case _ if income <= 201050 => (23200 * 0.10) + (94300 - 23200) * 0.12 + (income - 94300) * 0.22
-      case _ if income <= 383900 => (23200 * 0.10) + (94300 - 23200) * 0.12 + (201050 - 94300) * 0.22 + (income - 201050) * 0.24
-      case _ if income <= 487450 => (23200 * 0.10) + (94300 - 23200) * 0.12 + (201050 - 94300) * 0.22 + (383900 - 201050) * 0.24 + (income - 383900) * 0.32
-      case _ if income <= 731200 => (23200 * 0.10) + (94300 - 23200) * 0.12 + (201050 - 94300) * 0.22 + (383900 - 201050) * 0.24 + (487450 - 383900) * 0.32 + (income - 487450) * 0.35
-      case _ => (23200 * 0.10) + (94300 - 23200) * 0.12 + (201050 - 94300) * 0.22 + (383900 - 201050) * 0.24 + (487450 - 383900) * 0.32 + (731200 - 487450) * 0.35 + (income - 731200) * 0.37
+  private case class TaxBracket(upperLimit: Double, rate: Double)
+
+  private val taxBrackets: List[TaxBracket] = List(
+    TaxBracket(23200, 0.10),
+    TaxBracket(94300, 0.12),
+    TaxBracket(201050, 0.22),
+    TaxBracket(383900, 0.24),
+    TaxBracket(487450, 0.32),
+    TaxBracket(731200, 0.35),
+    TaxBracket(Double.MaxValue, 0.37)
+  )
+
+  private def calculateTax(income: Double): Double = {
+    var tax = 0.0
+    var previousLimit = 0.0
+
+    breakable {
+      for (bracket <- taxBrackets) {
+        if (income > bracket.upperLimit) {
+          tax += (bracket.upperLimit - previousLimit) * bracket.rate
+          previousLimit = bracket.upperLimit
+        } else {
+          tax += (income - previousLimit) * bracket.rate
+          break()
+        }
+      }
     }
+    tax
   }
 
   def main(args: Array[String]): Unit = {
